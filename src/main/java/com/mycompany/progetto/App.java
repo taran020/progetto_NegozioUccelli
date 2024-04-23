3f724087f9e44e739536120fa47c428a4364a4b4
taran <taran@LAPTOP-LBU49RB6>
2024-04-23 20:19:06 +0200

    inizio progetto

---
+++ src/main/java/com/mycompany/progetto/App.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progetto;
+
+/**
+ *
+ * @author taran
+ */
+public class App 
+{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
